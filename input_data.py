9fa36351de06165bb98462021d3d8795a07cd77a
Paola Chicaiza <pao.paty.chicaiza@gmail.com>
2022-06-16 19:09:02 -0500

    add class input_data for reading and parse data

---
+++ input_data.py
@@ -0,0 +1,26 @@
+class InputData:
+    def __init__(self):
+        self.lines = []
+        self.employee_worked_hours = {}
+
+    def read_data(self,filepath):
+        with open(filepath) as f:
+            lines = f.readlines()
+        for line in lines:
+            self.lines.append(line.strip())
+
+    def parse_lines(self):
+        for line in self.lines:
+            intervals = []
+            tokenized_line = line.split("=")
+            tokenized_worked_hours = list(tokenized_line[1].split(","))
+
+            for tokenized_worked_hour in tokenized_worked_hours:
+                day = tokenized_worked_hour[0:2]
+                start_time = int(tokenized_worked_hour[2:4])
+                end_time = int(tokenized_worked_hour[8:10])
+                for i in range(start_time,end_time):
+                    interval = f"{day}{i}:00-{i+1}:00"
+                    intervals.append(interval)
+            self.employee_worked_hours[tokenized_line[0]] = intervals
+        print(type(self.employee_worked_hours))
---
+++ tests/test_input_data.py
@@ -0,0 +1,22 @@
+import unittest
+
+from input_data import InputData
+
+
+class TestInputData(unittest.TestCase):
+    def test_read_data(self):
+        input_data = InputData()
+        input_data.read_data("mock_data/test_data.txt")
+        actual = input_data.lines
+        self.assertTrue(len(actual)>0)
+
+    def test_parse_lines(self):
+        input_data = InputData()
+        input_data.read_data("mock_data/test_data.txt")
+        input_data.parse_lines()
+        actual = input_data.employee_worked_hours
+        expected = {'RENE': ['MO10:00-11:00', 'MO11:00-12:00', 'TU10:00-11:00', 'TU11:00-12:00', 'TH1:00-2:00', 'TH2:00-3:00', 'SA14:00-15:00', 'SA15:00-16:00', 'SA16:00-17:00', 'SA17:00-18:00', 'SU20:00-21:00'],'ASTRID': ['MO10:00-11:00', 'MO11:00-12:00', 'TH12:00-13:00', 'TH13:00-14:00', 'SU20:00-21:00']}
+        self.assertDictEqual(actual, expected)
+
+if __name__ == '__main__':
+    unittest.main()
